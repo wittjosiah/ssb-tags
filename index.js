3479602797730051f81055a1a2f07b7b3fc77fb8
Josiah Witt <josiah@witt.life>
2018-06-18 22:09:49 -0400

    Add tagged message functions

--- index.js
+++ index.js
@@ -1,23 +1,115 @@
 var FlumeReduce = require('flumeview-reduce')
+var pull = require('pull-stream')
 var ref = require('ssb-ref')
 var get = require('lodash/get')
 var merge = require('lodash/merge')
+var map = require('lodash/map')
+var sortBy = require('lodash/sortBy')
+var entries = require('lodash/entries')
+var has = require('lodash/has')
 var TagHelper = require('scuttle-tag')
 
 exports.name = 'tags'
 exports.version = require('./package.json').version
 exports.manifest = {
   stream: 'source',
-  get: 'async'
+  get: 'async',
+  streamTaggedMessages: 'source',
+  getTaggedMessages: 'async',
+  streamMessagesTaggedWith: 'source',
+  getMessagesTaggedWith: 'async'
 }
 
 var initialState = {}
 
 exports.init = function (ssb, config) {
   var ScuttleTag = TagHelper(ssb)
-  return ssb._flumeUse('tags', FlumeReduce(2, reduce, map, null, initialState))
+  var view = ssb._flumeUse('tags', FlumeReduce(3, tagsReduce, tagsMap, null, initialState))
 
-  function reduce (result, item) {
+  return {
+    stream: view.stream,
+    get: view.get,
+    streamTaggedMessages,
+    getTaggedMessages,
+    streamMessagesTaggedWith,
+    getMessagesTaggedWith
+  }
+
+  function getTaggedMessages (cb) {
+    view.get((err, data) => {
+      if (err) cb(err)
+      var messages = {}
+      for (var author in data) {
+        for (var tag in data[author]) {
+          for (var msg in data[author][tag]) {
+            var timestamp = data[author][tag][msg]
+            if (!messages[msg] || messages[msg] < timestamp) messages[msg] = timestamp
+          }
+        }
+      }
+      var sortedMsgIds = map(sortBy(entries(messages), ([msgId, timestamp]) => -timestamp), 0)
+      cb(null, sortedMsgIds)
+    })
+  }
+
+  function streamTaggedMessages (opts) {
+    return pull(
+      view.stream(opts),
+      pull.map((item) => {
+        if (item.tagKey) return item
+        var messages = {}
+        for (var author in item) {
+          for (var tag in item[author]) {
+            for (var msg in item[author][tag]) {
+              var timestamp = item[author][tag][msg]
+              if (!messages[msg] || messages[msg] < timestamp) messages[msg] = timestamp
+            }
+          }
+        }
+        return map(sortBy(entries(messages), ([msgId, timestamp]) => -timestamp), 0)
+      })
+    )
+  }
+
+  function getMessagesTaggedWith (tagId, cb) {
+    view.get((err, data) => {
+      if (err) cb(err)
+      var messages = {}
+      for (var author in data) {
+        for (var msg in data[author][tagId]) {
+          var timestamp = data[author][tagId][msg]
+          if (!messages[msg] || messages[msg] < timestamp) messages[msg] = timestamp
+        }
+      }
+      var sortedMsgIds = map(sortBy(entries(messages), ([msgId, timestamp]) => -timestamp), 0)
+      cb(null, sortedMsgIds)
+    })
+  }
+
+  function streamMessagesTaggedWith (tagId, opts) {
+    return pull(
+      view.stream(opts),
+      pull.filter((item) => {
+        // if `tagKey` property does not exist `item` is the index
+        // which needs to be reduced
+        var live = has(item, 'tagKey')
+        return !live || item.tagKey === tagId
+      }),
+      pull.map((item) => {
+        if (item.tagKey) return item
+        var messages = {}
+        for (var author in item) {
+          for (var msg in item[author][tagId]) {
+            var timestamp = item[author][tagId][msg]
+            if (!messages[msg] || messages[msg] < timestamp) messages[msg] = timestamp
+          }
+        }
+        return map(sortBy(entries(messages), ([msgId, timestamp]) => -timestamp), 0)
+      })
+    )
+  }
+
+  function tagsReduce (result, item) {
     if (!item) return result
 
     var { root, tagKey, author, message, tagged, timestamp } = item
@@ -48,7 +140,7 @@ exports.init = function (ssb, config) {
     return result
   }
 
-  function map (msg) {
+  function tagsMap (msg) {
     // unbox private message
     if (msg.value.content === 'string') {
       // unbox private message (requires ssb-private plugin)
